908fa9bbfdb7186f32f131f7632c7ebc0eb04f62
ElisabettaMirti <belmirti@gmail.com>
2024-08-26 17:51:57 +0200

    configurazione progetto e esguita correttamente chiamata get dell'api

---
+++ src/components/AppMain.vue
@@ -0,0 +1,47 @@
+<script>
+import axios from 'axios';
+
+export default {
+    data(){
+        return {
+            projects : [],
+        }
+    },
+    methods: {
+        getProjects(){
+            axios.get('http://127.0.0.1:8000/api/projects', {
+                params: {
+                
+                }
+            })
+            .then((response) => {
+                console.log(response.data.results);
+                this.projects = response.data.results; 
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
+        }
+    },
+    created(){
+        this.getProjects();
+    }
+}
+
+</script>
+
+<template>
+    <h1>main content</h1>
+    <ul>
+        <li v-for="project in projects">
+            {{ project.title }}
+        </li>
+    </ul>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    }
+}
+
+</script>
+
+<template>
+<h1>main header</h1>
+</template>
+
+<style>
+
+</style>
